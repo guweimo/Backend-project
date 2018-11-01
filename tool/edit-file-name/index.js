b28dee97718d79540209ff2c2519e8426c407d0d
guweimo <lyfw244885@hotmail.com>
2018-11-01 22:00:44 +0800

    更改文件名字。

--- tool/edit-file-name/index.js
+++ tool/edit-file-name/index.js
@@ -1,10 +1,23 @@
 const fs = require('fs');
+const path = require('path');
 
-let load = 'C:\\Users\\lyfw2\\OneDrive\\Photos';
+let load = 'D:\\test';
 
-let files = fs.readdir(load, (err, file)=> {
-    if (file.indexOf('SPSCF') == -1) {
-    } else {
-        console.log(file);
+let files = fs.readdir(load, (err, files)=> {
+    if (err) {
+        console.warn(err);
+        return false;
+    }
+    for (let file of files) {
+        if (file.includes('SPSCF')) {
+            let oldName = path.join(load, file);
+            let fileType = path.parse(file).ext;
+            let fileName = path.parse(file).name;
+            let newName = 'CHD' + fileName.slice(5) + fileType;
+            newName = path.join(load, newName);
+            fs.rename(oldName, newName, (err) => {
+                if (err) throw err;
+            });
+        }
     }
 });
